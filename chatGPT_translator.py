447130bf7e817a044d404a67c575cc57c7b6b061
hwdong-net <53254679+hwdong-net@users.noreply.github.com>
2023-04-13 17:02:36 +0800

    Update chatGPT_translator.py

--- chatGPT_translator.py
+++ chatGPT_translator.py
@@ -67,5 +67,7 @@ if __name__ == "__main__":
     input_path = args.input_file
     output_path = args.output_file
 
+    print("input_path:",input_path)
+    print("output_path:",output_path)
     # Translate the input file and save the translated text to the output file
     translate_file(input_path, output_path, args.model, args.openai_key)
