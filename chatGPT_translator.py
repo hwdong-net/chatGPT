b938045746d816baa20da89014bebced2e22862b
hwdong-net <53254679+hwdong-net@users.noreply.github.com>
2023-04-13 16:53:55 +0800

    Update chatGPT_translator.py

--- chatGPT_translator.py
+++ chatGPT_translator.py
@@ -62,8 +62,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Get the full path of the input and output files
-    input_path = '/content/drive/MyDrive/data' + args.input_file
-    output_path = '/content/drive/MyDrive/data' + args.output_file
+    #input_path = '/content/drive/MyDrive/data' + args.input_file
+    #output_path = '/content/drive/MyDrive/data' + args.output_file
+    input_path = args.input_file
+    output_path = args.output_file
 
     # Translate the input file and save the translated text to the output file
     translate_file(input_path, output_path, args.model, args.openai_key)
